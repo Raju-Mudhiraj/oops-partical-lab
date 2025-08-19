48a4d9489d59fc21741bddf993bf791e1cf39bce
Raju-Mudhiraj <nagarajumudhiraj34@gmail.com>
2025-08-19 13:32:39 +0530

    Add files via upload

---
+++ Overloading_C.java
@@ -0,0 +1,51 @@
+package oop;
+class BoxOver
+ { 
+   double width;
+   double height; 
+   double depth; 
+   BoxOver(double w, double h, double d) 
+   {
+	width = w;
+	height = h; 
+	depth = d;
+    } 
+
+BoxOver() 
+ {
+   width = -1;  
+   height = -1; 
+   depth = -1; 
+  } 
+ 
+     BoxOver(double len) 
+{
+ width = height = depth = len;
+ } 
+
+double volume() 
+  {
+ 	return width * height * depth;
+  }
+ } 
+class Overloading_C
+{
+ public static void main(String args[]) 
+{ 
+   
+    BoxOver mybox1 = new BoxOver(10, 20, 15); 
+    BoxOver mybox2 = new BoxOver(); 
+    BoxOver mycube = new BoxOver(7); 
+    double vol; 
+
+    vol = mybox1.volume();
+    System.out.println("Volume of mybox1 is " + vol); 
+
+   vol = mybox2.volume(); 
+   System.out.println("Volume of mybox2 is " + vol); 
+
+  vol = mycube.volume();
+   System.out.println("Volume of mycube is " + vol); 
+}
+ }
+
